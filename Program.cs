d664bac4d511a2183109d46524c0c4ca7021f482
Michael Wood <ikelw2@gmail.com>
2026-07-15 17:15:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// begin with pseudocode
+
+// =========================================================================
+// 1.Write a program in C# Sharp to find the sum of all array elements.
+// Test Data :
+//   Input the number of elements to be stored in the array :3
+//   Input 3 elements in the array :
+//   element - 0 : 2
+//   element - 1 : 5
+//   element - 2 : 8
+// Expected Output :
+//   Sum of all elements stored in the array is : 15
+
+
+
+// =========================================================================
+// 2.Write a C# Sharp program to find the largest of three numbers.
+// Test Data :
+//   Input the 1st number :25
+//   Input the 2nd number :63
+//   Input the 3rd number :10
+// Expected Output :
+//   The 2nd Number is the greatest among three
+
+
