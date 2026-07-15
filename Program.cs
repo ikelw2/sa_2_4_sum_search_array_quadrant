58326b8f8935c25f31f5d8a7b7be14f6947eab42
Michael Wood <ikelw2@gmail.com>
2026-07-15 20:34:44 +0200

    added trig Atan2 solution to calculate quadrant

--- Program.cs
+++ Program.cs
@@ -81,71 +81,103 @@ if (myArrayOps.LoadElementsIntoArray()) // if Load Element Operation successful
 // 3. Write a C# Sharp program to accept a coordinate point in an XY coordinate
 //    system and determine in which quadrant the coordinate point lies.
 
-Console.WriteLine("\nEnter XY point coordinates to determine in which quadrant it lies.");
-Console.Write("Enter a double value for the X coordinate (left-right) --> ");
-double xCoord = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter a double value for the Y coordinate (up-down) --> ");
-double yCoord = Convert.ToDouble(Console.ReadLine());
-
-// a poss improved method to determine this would be to use geometric math
-
-// new solution using trig, after researching the function online
-
-// 1. Calculate angle from origin to x-y point (output in radians)
-double angleInRadians = Math.Atan2(yCoord, xCoord);
-
-angleInRadians %= Math.PI; // remove any excess PIs to keep it within range for our switch function
-
-string quadrant = angleInRadians switch 
+//Console.WriteLine("\nEnter XY point coordinates to determine in which quadrant it lies.");
+while (true)
 {
-    >= 0 and < (Math.PI/2) => "I",
-    >= (Math.PI/2) and < (Math.PI) => "II",
-    >= (Math.PI) and < ( (3 * Math.PI) / 2) => "III",
-    >= ( (3 * Math.PI) / 2) => "IV"
-};
+    //Console.Clear();
+    Console.WriteLine("\n-------------------");
+    Console.Write("Enter a double value for the X coordinate (left-right) --> ");
+    double xCoord = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter a double value for the Y coordinate (up-down) --> ");
+    double yCoord = Convert.ToDouble(Console.ReadLine());
+
+    // a poss improved method to determine this would be to use geometric math
 
 
-// old solution using logic and conditions
 
-// // part 1: use logic to determine top/bottom and left/right variance
-// bool isTop = false;
-// if (yCoord >= 0)
-// {
-//     isTop = true;
-// }
-// bool isRight = false;
-// if (xCoord >= 0)
-// {
-//     isRight = true;
-// }
 
-// // part 2: use logic following isTop and isRight to assign quadrant
-// string quadrant = "undetermined";
-// if (isTop) 
-// {
-//     if (isRight)
-//     {
-//         quadrant = "I";
-//     }
-//     if (!isRight)
-//     {
-//         quadrant = "II";
-//     }
-// }
-// else // isTop == false
-// {
-//     if (isRight)
-//     {
-//         quadrant = "III";
-//     }
-//     if (!isRight)
-//     {
-//         quadrant = "IV";
-//     }
-// }
+    // new solution using trig, after researching the function online
 
-Console.WriteLine($"\nThe XY point at ({xCoord}, {yCoord}) is in quadrant {quadrant}.\n");
+    // 1. Calculate angle from origin to x-y point (output in radians)
+    int angleInDegrees = Convert.ToInt32(Math.Atan2(yCoord, xCoord) * (180 / Math.PI));
+    // Console.WriteLine("before:  angleInDegrees = " + angleInDegrees);
 
+    if (angleInDegrees < 0)  // required adjustment because Atan2 returns between -180 to +180 in degrees
+    {
+        angleInDegrees += 360;
+    }
+    //Console.WriteLine("after:  angleInDegrees = " + angleInDegrees + "      to get quad I take angle / 90");
+    int quadrant = Convert.ToInt32((angleInDegrees) / 90) + 1;
+
+
+    // notes for sharing
+    //double angleFromOriginToPointInRadians = Math.Atan2(yCoord, xCoord);
+    //double angleFromOriginToPointInDegrees = angleFromOriginToPointInRadians * (180 / Math.PI);
+    //int angleInDegreesAsInteger = Convert.ToInt32(angleFromOriginToPointInDegrees);
+    //if (angleInDegreesAsInteger < 0)
+    //{
+    //        angleInDegreesAsInteger += 360; // required conditional because Atan2 returns between -180 to +180 in degrees, not between 0 and 360 degrees
+    //}
+    //int quadrant = ((angleInDegreesAsInteger) / 90) + 1; // add one because result returns between 0 and 3, we want between 1 and 4
+
+
+
+
+
+
+
+
+    // old solution using logic and conditions-----------------
+
+    // // part 1: use logic to determine top/bottom and left/right variance
+    // bool isTop = false;
+    // if (yCoord >= 0)
+    // {
+    //     isTop = true;
+    // }
+    // bool isRight = false;
+    // if (xCoord >= 0)
+    // {
+    //     isRight = true;
+    // }
+
+    // // part 2: use logic following isTop and isRight to assign quadrant
+    // string quadrant = "undetermined";
+    // if (isTop) 
+    // {
+    //     if (isRight)
+    //     {
+    //         quadrant = "I";
+    //     }
+    //     if (!isRight)
+    //     {
+    //         quadrant = "II";
+    //     }
+    // }
+    // else // isTop == false
+    // {
+    //     if (isRight)
+    //     {
+    //         quadrant = "III";
+    //     }
+    //     if (!isRight)
+    //     {
+    //         quadrant = "IV";
+    //     }
+    // }
+
+    Console.WriteLine($"The XY point at ({xCoord}, {yCoord}) is in quadrant {quadrant}.");
+
+    Console.Write("Press Q to quit or ENTER to test the function again.");
+    
+    string repeatAgain = Console.ReadLine();
+    if (repeatAgain.Trim().Equals("q", StringComparison.OrdinalIgnoreCase) == true)
+    {
+        break;
+    }
+
+
+}
 // Test Data :
 // Input the value for X coordinate :7
 // Input the value for Y coordinate :9
@@ -159,7 +191,7 @@ Console.WriteLine($"\nThe XY point at ({xCoord}, {yCoord}) is in quadrant {quadr
 #region ShutdownTasks
 // ------------------------------------------------------------------------
 // End of Code, wait for user to press enter, and clear background
-Console.ReadLine();
+//Console.ReadLine();  // redundant since in 3rd part user already asked if they want to quit etc
 AnsiConsole.Clear();
 //
 #endregion
